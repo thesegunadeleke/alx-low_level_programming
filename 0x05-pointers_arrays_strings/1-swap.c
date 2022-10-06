1afe2ee450dc58ca62d463dc7c48f7f2544fd1f2
thesegunadeleke <thesegunadeleke@gmail.com>
2022-10-06 15:12:12 -0700

    Correct betty error

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
+/**
+ * swap_int - function to swap
+ * values of 2 integers
+ * @a: first value
+ * @b: second value
+ * Return: success
+*/
+
 void swap_int(int *a, int *b)
 {
 	int t;
+
 	t = *a;
 	*a = *b;
 	*b = t;
-    
 }
